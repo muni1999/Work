7776e8809d76b5e18c015fa8db3a2a0fbd71af5e
munireddy <munireddy6666@gmail.com>
2025-10-21 14:09:36 -0400

    Added more variables to code

--- Week1/Day1/TASK1/TASK1/Program.cs
+++ Week1/Day1/TASK1/TASK1/Program.cs
@@ -5,6 +5,22 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            int age = 25;                
+            string name = "Muni";            
+            double height = 6;             
+            bool isStudent = true;           
+            char grade = 'A';               
+
+           
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Height: " + height);
+            Console.WriteLine("Is Student: " + isStudent);
+            Console.WriteLine("Grade: " + grade);
+
+          
+           
         }
     }
 }
