9ca78e753be6ffadc87d86ffbea01c282127df9e
munireddy <munireddy6666@gmail.com>
2025-10-21 14:16:00 -0400

    Taking user input and showing it

---
+++ Week1/Day1/Task3/Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nHello, " + name);
+            Console.WriteLine("You are " + age);
+        }
+    }
+}
