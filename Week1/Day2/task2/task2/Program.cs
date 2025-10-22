7814a8166a82d089bc4033ba94b6bcdebcff587f
munireddy <munireddy6666@gmail.com>
2025-10-21 22:54:14 -0400

    Area of rectangle

---
+++ Week1/Day2/task2/task2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the length of the rectangle: ");
+            double length = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the breadth of the rectangle: ");
+            double breadth = Convert.ToDouble(Console.ReadLine());
+            double area = length * breadth;
+
+            Console.WriteLine($"\n area of the rectangle is: {area}");
+        }
+    }
+}
