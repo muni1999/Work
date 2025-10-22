3ae96345ca2bfa94540e8ba21242b045a1bfb668
munireddy <munireddy6666@gmail.com>
2025-10-21 23:01:03 -0400

    convert c to F

---
+++ Week1/Day2/Task3/Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter c =");
+            double C = Convert.ToDouble(Console.ReadLine());
+
+            double F = (C * 9 / 5) + 32;
+            Console.WriteLine(F);
+
+        }
+    }
+}
