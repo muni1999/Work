bb2e9fa17e48a98abf9ad62489149932a2015e35
munireddy <munireddy6666@gmail.com>
2025-10-22 21:24:16 -0400

    largest of three

---
+++ Week1/Day3/Task 1/Task 1/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("first number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter second number:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter third number:");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            int largest;
+
+            if (num1 >= num2 && num1 >= num3)
+            {
+                largest = num1;
+            }
+            else if (num2 >= num1 && num2 >= num3)
+            {
+                largest = num2;
+            }
+            else
+            {
+                largest = num3;
+            }
+
+            Console.WriteLine("The largest number is: " + largest);
+        }
+    }
+}
