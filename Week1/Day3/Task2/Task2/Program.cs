0556dd422c3961b71e062f2f84cd856f5483eb41
munireddy <munireddy6666@gmail.com>
2025-10-22 21:35:59 -0400

    odd or even

---
+++ Week1/Day3/Task2/Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           double num = Convert.ToDouble(Console.ReadLine());
+            if (num%2== 0)
+            { 
+                Console.WriteLine("even");
+            }
+            else
+            {
+                Console.WriteLine("odd");
+            }
+
+
+
+
+        }
+    }
+}
