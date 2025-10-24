07aaf8bbb9b43283528b115990ffe46a2602756d
munireddy <munireddy6666@gmail.com>
2025-10-23 21:47:28 -0400

    tables

---
+++ Week1/Day4/task3/task3/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"\nMultiplication Table of {num}:");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{num} x {i} = {num * i}");
+            }
+        }
+    }
+}
