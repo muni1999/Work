a3646358fa06dd7ba3999c9919e27c202f845f06
munireddy <munireddy6666@gmail.com>
2025-10-28 09:27:40 -0400

    F2

---
+++ Week2/day1/F2/F2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace F2
+{
+    internal class Program
+    {
+        static int MaxOfThree(int a, int b, int c)
+        {
+            int max = a; 
+
+            if (b > max)
+                max = b;
+            if (c > max)
+                max = c;
+
+            return max;
+        }
+
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine($"Max of (10, 20, 15) = {MaxOfThree(10, 20, 15)}");
+            Console.WriteLine($"Max of (5, 3, 9) = {MaxOfThree(5, 3, 9)}");
+            Console.WriteLine($"Max of (-4, -7, -2) = {MaxOfThree(-4, -7, -2)}");
+            Console.WriteLine($"Max of (100, 100, 100) = {MaxOfThree(100, 100, 100)}");
+        }
+    }
+}
