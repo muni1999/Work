db8b0cf1cc58eac43dd783d22ba2dd3c66971daf
munireddy <munireddy6666@gmail.com>
2025-10-28 09:35:48 -0400

    square

---
+++ Week2/day1/F3/F3/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace F3
+{
+    internal class Program
+    {
+        
+            static int Square(int n)
+            {
+                return n * n;   
+            }
+
+            static void Main(string[] args)
+            {
+                
+                Console.Write("Enter a number: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+
+                int result = Square(num);
+
+              
+                Console.WriteLine($"Square of {num} = {result}");
+              
+            }
+        
+    }
+}
