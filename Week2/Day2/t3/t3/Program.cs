1b66b0a56b4a442a2888b67c113f0c45bac545bb
munireddy <munireddy6666@gmail.com>
2025-10-29 10:09:26 -0400

    large no

---
+++ Week2/Day2/t3/t3/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace t3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numbers = new int[5];
+
+            Console.WriteLine("Enter 5 integers:");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($"Element {i + 1}: ");
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+          
+            int largest = numbers[0];
+
+            
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] > largest)
+                {
+                    largest = numbers[i];
+                }
+            }
+
+            Console.WriteLine($"\nThe largest element in the array is: {largest}");
+
+            Console.ReadLine();
+        }
+    }
+}
