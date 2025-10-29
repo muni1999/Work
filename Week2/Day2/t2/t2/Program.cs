b6aa2562e2cadf28b42d2387aeefd9729043754a
munireddy <munireddy6666@gmail.com>
2025-10-29 10:07:18 -0400

    addd up

---
+++ Week2/Day2/t2/t2/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace t2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] numbers = new int[5];
+
+            
+            Console.WriteLine("Enter 5 integers:");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($"Element {i + 1}: ");
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+
+          
+            Console.WriteLine($"\nThe sum of all elements = {sum}");
+
+            Console.ReadLine();
+        }
+    }
+}
