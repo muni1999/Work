3cc74f5a5be74c0d6f14d111c2439745159e1015
munireddy <munireddy6666@gmail.com>
2025-10-30 22:48:46 -0400

    matrix

---
+++ Week2/Day2/Matrix/Matrix/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[2, 2];
+
+            Console.WriteLine("Enter elements for a 2x2 matrix:");
+
+        
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write($"Enter element [{i + 1},{j + 1}]: ");
+                    matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("\nThe 2x2 Matrix is:\n");
+
+           
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
