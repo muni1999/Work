10fdfd09d35d7fd26bb369f78e9e2ab38e1bac91
munireddy <munireddy6666@gmail.com>
2025-10-30 22:51:23 -0400

    sum of matrix

---
+++ Week2/Day2/matrix ssum/matrix ssum/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace matrix_ssum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[2, 2];
+
+            Console.WriteLine("Enter elements for a 2x2 matrix:");
+
+            // Taking input from user
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write($"Enter element [{i + 1},{j + 1}]: ");
+                    matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("\nThe 2x2 Matrix is:\n");
+
+            // Display matrix
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\nSum of each row:");
+
+            // Calculate sum of each row
+            for (int i = 0; i < 2; i++)
+            {
+                int rowSum = 0;
+                for (int j = 0; j < 2; j++)
+                {
+                    rowSum += matrix[i, j];
+                }
+                Console.WriteLine($"Sum of row {i + 1}: {rowSum}");
+            }
+        }
+    }
+}
