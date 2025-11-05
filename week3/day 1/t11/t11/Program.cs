1fe02882cf7fcd0e305f167fb5d3df7766b90ea4
munireddy <munireddy6666@gmail.com>
2025-11-05 09:30:28 -0500

    hi

---
+++ week3/day 1/t11/t11/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace EmployeeApp
+{
+    class Employee
+    {
+        // Private fields
+        private int empId;
+        private string name;
+        private double salary;
+
+        // Public properties
+        public int EmpId
+        {
+            get { return empId; }
+            set { empId = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public double Salary
+        {
+            get { return salary; }
+            set { salary = value; }
+        }
+
+        // Method to display employee info
+        public void DisplayInfo()
+        {
+            Console.WriteLine("\n--- Employee Details ---");
+            Console.WriteLine($"Employee ID: {EmpId}");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Salary: {Salary}");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Employee emp = new Employee();
+
+            Console.Write("Enter Employee ID: ");
+            emp.EmpId = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Employee Name: ");
+            emp.Name = Console.ReadLine();
+
+            Console.Write("Enter Employee Salary: ");
+            emp.Salary = Convert.ToDouble(Console.ReadLine());
+
+            
+            emp.DisplayInfo();
+
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ week3/day 1/week2 exam/week2 exam/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+
+public class Program
+{
+    static int Add(int a, int b) => a + b;
+
+    static int MaxOfThree(int a, int b, int c)
+    {
+        if (a >= b && a >= c) return a;
+        else if (b >= a && b >= c) return b;
+        else return c;
+    }
+
+    static int Square(int n) => n * n;
+
+    static void CreateAndDisplayArray()
+    {
+        Console.Write("Enter size: ");
+        int size = Convert.ToInt32(Console.ReadLine());
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) arr[i] = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Array:");
+        foreach (int item in arr) Console.Write(item + " ");
+        Console.WriteLine();
+    }
+
+    static void SumOfArrayElements()
+    {
+        Console.Write("Enter size: ");
+        int size = Convert.ToInt32(Console.ReadLine());
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) arr[i] = Convert.ToInt32(Console.ReadLine());
+        int sum = 0;
+        foreach (int val in arr) sum += val;
+        Console.WriteLine("Sum = " + sum);
+    }
+
+    static void ReverseArray()
+    {
+        Console.Write("Enter size: ");
+        int size = Convert.ToInt32(Console.ReadLine());
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) arr[i] = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Reversed array:");
+        for (int i = size - 1; i >= 0; i--) Console.Write(arr[i] + " ");
+        Console.WriteLine();
+    }
+
+    static void FindLargestElement()
+    {
+        Console.Write("Enter size: ");
+        int size = Convert.ToInt32(Console.ReadLine());
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) arr[i] = Convert.ToInt32(Console.ReadLine());
+        int max = arr[0];
+        for (int i = 1; i < size; i++) if (arr[i] > max) max = arr[i];
+        Console.WriteLine("Largest element = " + max);
+    }
+
+    static void TransposeMatrix()
+    {
+        Console.Write("Enter rows: ");
+        int rows = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter columns: ");
+        int cols = Convert.ToInt32(Console.ReadLine());
+        int[,] matrix = new int[rows, cols];
+        Console.WriteLine("Enter elements:");
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < cols; j++)
+                matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Transpose:");
+        for (int i = 0; i < cols; i++)
+        {
+            for (int j = 0; j < rows; j++)
+                Console.Write(matrix[j, i] + " ");
+            Console.WriteLine();
+        }
+    }
+
+    public static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("1 Add nos ");
+            Console.WriteLine("2 big ofThree");
+            Console.WriteLine("3Square of no");
+            Console.WriteLine("4Create and Display Array");
+            Console.WriteLine("5Sum of Array Elements");
+            Console.WriteLine("6Reverse Array");
+            Console.WriteLine("7Find Largest Element");
+            Console.WriteLine("8Transpose of Matrix");
+            Console.WriteLine("9Exit");
+            Console.Write("Enter your choice: ");
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Console.Write("Enter two nos: ");
+                    Console.WriteLine("Sum = " + Add(Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())));
+                    break;
+                case 2:
+                    Console.Write("Enter three nos: ");
+                    Console.WriteLine("Max = " + MaxOfThree(Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())));
+                    break;
+                case 3:
+                    Console.Write("Enter nos: ");
+                    Console.WriteLine("Square = " + Square(Convert.ToInt32(Console.ReadLine())));
+                    break;
+                case 4:
+                    CreateAndDisplayArray();
+                    break;
+                case 5:
+                    SumOfArrayElements();
+                    break;
+                case 6:
+                    ReverseArray();
+                    break;
+                case 7:
+                    FindLargestElement();
+                    break;
+                case 8:
+                    TransposeMatrix();
+                    break;
+                case 9:
+                    return;
+                default:
+                    Console.WriteLine("Invalid choice.");
+                    break;
+            }
+        }
+    }
+}
