2eaf6ebc762227d7448577a5471ca064c50f520a
munireddy <munireddy6666@gmail.com>
2025-11-06 09:23:13 -0500

    HI

---
+++ week3/day4/D4t1/D4t1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace D4t1
+{
+    
+        public interface IPrintable
+        {
+            void Print();
+        }
+
+        
+        public class Document : IPrintable
+        {
+            public void Print()
+            {
+                Console.WriteLine("Printing document details...");
+            }
+        }
+
+      
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                IPrintable printable = new Document();
+                printable.Print();
+
+                Console.ReadLine();
+            }
+        }
+    
+}
