02cbd8483906706c79e5ad49b17bc3cef6399f72
munireddy <munireddy6666@gmail.com>
2025-11-06 09:25:10 -0500

    hi

---
+++ week3/day4/D4t2/D4t2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace D4t2
+{
+    
+    public abstract class Shape
+    {
+        
+        public abstract double Area();
+    }
+
+    public class Rectangle : Shape
+    {
+        private double length;
+        private double width;
+
+        public Rectangle(double length, double width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+       
+        public override double Area()
+        {
+            return length * width;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle(10.5, 5.2);
+            Console.WriteLine("Area of Rectangle: " + rect.Area());
+
+            Console.ReadLine();
+        }
+    }
+}
