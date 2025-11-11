41181b4f8788bbfefc37d92881c079297aca38b8
munireddy <munireddy6666@gmail.com>
2025-11-11 09:23:29 -0500

    H:I

---
+++ week4/day1/user input/user input/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace UserInputDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== User Information Program ===\n");
+
+          
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter your salary: ");
+            double salary = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter your city: ");
+            string city = Console.ReadLine();
+
+            Console.Clear();
+
+            Console.WriteLine("=== Employee Details ===\n");
+            Console.WriteLine($"Name     : {name}");
+            Console.WriteLine($"Age      : {age} years");
+            Console.WriteLine($"Salary   : {salary:C}"); 
+            Console.WriteLine($"City     : {city}");
+            Console.WriteLine("\nThank you for providing your details!");
+
+          
+            Console.ReadKey();
+        }
+    }
+}
