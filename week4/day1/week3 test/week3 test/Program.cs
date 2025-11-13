a37dc161ad7530a4b939c467a0a30efc241b97b7
munireddy <munireddy6666@gmail.com>
2025-11-13 08:57:24 -0500

    void

---
+++ week4/day1/week3 test/week3 test/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+interface IPrintable
+{
+    void PrintDetails();
+}
+
+abstract class Person : IPrintable
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    public abstract void Work();
+
+    public virtual void PrintDetails()
+    {
+        Console.WriteLine($"Name: {Name}, Age: {Age}");
+    }
+}
+
+class Student : Person
+{
+    public string Course { get; set; }
+    public double GPA { get; set; }
+
+    public Student(string name, int age, string course, double gpa)
+        : base(name, age)
+    {
+        Course = course;
+        GPA = gpa;
+    }
+
+    public override void Work()
+    {
+        Console.WriteLine($"{Name} is studying for the {Course} course.");
+    }
+
+    public override void PrintDetails()
+    {
+        base.PrintDetails();
+        Console.WriteLine($"Course: {Course}, GPA: {GPA}");
+    }
+}
+
+class Employee : Person
+{
+    public string Department { get; set; }
+    public double Salary { get; set; }
+
+    public Employee(string name, int age, string department, double salary)
+        : base(name, age)
+    {
+        Department = department;
+        Salary = salary;
+    }
+    public override void Work()
+    {
+        Console.WriteLine($"{Name} is working in the {Department} department.");
+    }
+
+    public override void PrintDetails()
+    {
+        base.PrintDetails();
+        Console.WriteLine($"Department: {Department}, Salary: {Salary:C}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student s1 = new Student("Muni", 22, "Computer Science", 3.8);
+        Employee e1 = new Employee("Reddy", 30, "IT", 85000);
+
+        Person[] people = { s1, e1 };
+
+        foreach (Person p in people)
+        {
+            p.PrintDetails();
+            p.Work();
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ week4/day3/Task1/Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num1 = 10;
+        int num2 = 0;
+
+        try
+        {
+            int result = num1 / num2; 
+            Console.WriteLine("Result: " + result);
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("❌ Cannot divide a number by zero. Please check your inputs.");
+        }
+        finally
+        {
+            Console.WriteLine("➡️ Program execution completed (finally block).");
+        }
+    }
+}
