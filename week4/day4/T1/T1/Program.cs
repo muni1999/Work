812febe927e31dec908302a5309cf99bb23ca23c
munireddy <munireddy6666@gmail.com>
2025-11-14 11:27:54 -0500

    data

---
+++ week4/day4/T1/T1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace Day4_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter some text to save into a file: ");
+            string userInput = Console.ReadLine();
+
+            // File name & path
+            string filePath = "UserInput.txt";
+
+            // Write user input to file
+            File.WriteAllText(filePath, userInput);
+
+            Console.WriteLine("\nData saved successfully into UserInput.txt");
+        }
+    }
+}
