50b31172d4b224fa12047eb2dec28cfd0e978fce
MuniReddy <munireddy6666@gmail.com>
2026-09-10 09:38:38 -0400

    argest NO

---
+++ metiz/week1/Day1/largest number/largest number/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter first number: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter third number: ");
+        double c = Convert.ToDouble(Console.ReadLine());
+
+        if (a >= b)
+        {
+            if (a >= c)
+            {
+                Console.WriteLine("Largest number is: " + a);
+            }
+            else
+            {
+                Console.WriteLine("Largest number is: " + c);
+            }
+        }
+        else
+        {
+            if (b >= c)
+            {
+                Console.WriteLine("Largest number is: " + b);
+            }
+            else
+            {
+                Console.WriteLine("Largest number is: " + c);
+            }
+        }
+    }
+}
