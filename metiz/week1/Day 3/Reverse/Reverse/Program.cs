05c6a8530ce00abff26e9b50e581d8391acb74ef
MuniReddy <munireddy6666@gmail.com>
2026-09-10 09:51:09 -0400

    reverse

---
+++ metiz/week1/Day 3/Reverse/Reverse/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        int reversed = 0;
+
+        while (number != 0)
+        {
+            int digit = number % 10;
+
+            reversed = reversed * 10 + digit;
+
+            number = number / 10;
+        }
+
+        Console.WriteLine("Reversed number: " + reversed);
+    }
+}
