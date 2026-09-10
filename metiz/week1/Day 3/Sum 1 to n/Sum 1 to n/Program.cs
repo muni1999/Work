d0d51a700550d814531306e1014ac721373f48fe
MuniReddy <munireddy6666@gmail.com>
2026-09-10 09:48:21 -0400

    sum

---
+++ metiz/week1/Day 3/Sum 1 to n/Sum 1 to n/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter N: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            sum = sum + i;
+        }
+
+        Console.WriteLine("Sum from 1 to " + n + " is: " + sum);
+    }
+}
