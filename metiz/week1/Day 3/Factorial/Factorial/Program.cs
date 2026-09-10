5b6b678edfee8aa04d2667bd67791dfc5b1b2ffb
MuniReddy <munireddy6666@gmail.com>
2026-09-10 09:49:53 -0400

    factorial

---
+++ metiz/week1/Day 3/Factorial/Factorial/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        long factorial = 1;
+
+        for (int i = 1; i <= number; i++)
+        {
+            factorial = factorial * i;
+        }
+
+        Console.WriteLine(number + "! = " + factorial);
+    }
+}
