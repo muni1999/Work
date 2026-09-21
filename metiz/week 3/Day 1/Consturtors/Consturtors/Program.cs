31235425a190e18f28f8edd7a2eb436adb7b60c9
MuniReddy <munireddy6666@gmail.com>
2026-09-21 10:41:18 -0400

    Constructor

---
+++ metiz/week 3/Day 1/Consturtors/Consturtors/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Consturtors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person defaultPerson = new Person();
+            Console.WriteLine($"Default Person: Name = {defaultPerson.Name}, Age = {defaultPerson.Age}");
+
+            
+            Person parameterizedPerson = new Person("John Doe", 30);
+            Console.WriteLine($"Parameterized Person: Name = {parameterizedPerson.Name}, Age = {parameterizedPerson.Age}");
+        }
+    }
+
+    internal class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person()
+        {
+            Name = "Unknown";
+            Age = 0;
+        
+
+        
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
