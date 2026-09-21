b35d6e98463a47be632a4da900a3c2d9da2d66a7
MuniReddy <munireddy6666@gmail.com>
2026-09-21 10:56:42 -0400

    day10 1

---
+++ metiz/week2/day 5/Day10 -1/Day10 -1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Day10__1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a sentence:");
+            string input = Console.ReadLine();
+
+            if (!string.IsNullOrWhiteSpace(input))
+            {
+                string[] words = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                Console.WriteLine($"Word count: {words.Length}");
+            }
+            else
+            {
+                Console.WriteLine("No input provided.");
+            }
+        }
+    }
+}
